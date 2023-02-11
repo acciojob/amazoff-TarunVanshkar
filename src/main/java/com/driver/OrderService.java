3aa26476248d719bf8e34cc633b7e50b4de2f0f5
TarunVanshkar <tarunben33@gmail.com>
2023-02-11 17:20:59 +0530

    Fourth_Commit

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class OrderService
 {
     @Autowired
-    OrderRepository orderRepository;
+    OrderRepository orderRepository=new OrderRepository();
 
     public String addOrder(Order order)
     {
